a04e4e33bb78a101bdea0f8286cb99d0f8b012da
print-happy <print_happy@outlook.com>
2024-03-31 11:50:34 +0800

    learning linear_regression

---
+++ Learning/2tensor.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+data_file = os.path.join('..', 'data', 'programmers.csv')
+data = pd.read_csv(data_file)
+inputs= data.iloc[:,:3]    #分段
+outputs= data.iloc[:,-1]    #分段
+inputs.drop(columns=['Programmers'],inplace=True)    #去除姓名行
+inputs = inputs.fillna(inputs.mean())    #用平均值填充缺失值
+#inputs.mean()返回一个Series，Series的索引是列名，值是平均值，传入fillna用于填充缺失值
+inputs = pd.get_dummies(inputs, dummy_na=True)    #将非数值型数据转换为数值型数据，并且使用one_hot编码转化成二进制向量
+import torch
+torch.set_printoptions(sci_mode=False)    #取消科学计数法（如果后面要用回来得设置为True）
+x = torch.tensor(inputs.to_numpy(dtype=float))    #将数据转化为张量
+y = torch.tensor(outputs.to_numpy(dtype=float))
+print(x,"\n")
+print(y)
+print(x.shape)
--- Learning/creatcsv.py
+++ Learning/creatcsv.py
@@ -3,12 +3,12 @@ os.makedirs(os.path.join('..', 'data'), exist_ok=True)
 data_file = os.path.join('..', 'data', 'programmers.csv')
 with open(data_file, 'w') as f:
     f.write('Programmers,salary,marriage,age\n') # 列名
-    f.write('A,10000,none,35\n') # 每行表示一个数据样本
+    f.write('A,10000,NaN,35\n') # 每行表示一个数据样本
     f.write('B,12000,married,37\n')
-    f.write('C,14000,none,30\n')
+    f.write('C,NaN,NaN,30\n')
     f.write('D,16000,married,40\n')
     f.write('E,18000,married,45\n')
-    f.write('F,20000,none,41\n')
+    f.write('F,20000,NaN,41\n')
     f.write('G,22000,married,50\n')
-    f.write('H,24000,none,55\n')
-    f.write('I,26000,married,60\n')    
+    f.write('H,24000,NaN,55\n')
+    f.write('I,26000,married,60\n')
